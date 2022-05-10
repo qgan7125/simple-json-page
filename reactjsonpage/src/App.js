dbf4e05ca5b0c58163b73876d2ba5e7a233eba4a
qgan7125 <qgan@email.arizona.edu>
2022-05-10 15:53:20 -0700

    Using prettyprint in react version

--- reactjsonpage/src/App.js
+++ reactjsonpage/src/App.js
@@ -1,54 +1,62 @@
 import './App.css';
 import _solr_base from './config';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
-import { JsonToTable } from 'react-json-to-table';
-
 
 function App() {
+  // Get the current URL parameter
   const { id } = useParams();
-
-  const [jsonData, setData] = useState();
-
-  async function fetchJSON(method = "GET") {
-    if (!id) {
-      setData("Please enter the identifier!");
-      return
-    }
-
-    const headers = { "Accept": "application/json" };
-    const url = _solr_base + encodeURIComponent(id) + `?format=full`;
-
-    try {
-      await fetch(url, {
-        method: method,
-        headers: headers
-      })
-        .then(res => res.json())
-        .then(data => {
-          setData(data)
-          const script = document.createElement('script');
-          script.type = "application/ld+json";
-          script.text = JSON.stringify(data, null, 4);
-          document.head.appendChild(script);
-        })
-    } catch (err) {
-      console.log("Error: ", err);
-      setData("Wrong Identifier!");
-    }
-  }
+  const container = useRef();
+  const [jsonData, setData] = useState("");
 
   useEffect(() => {
-    fetchJSON()
-  }, []);
+    /**
+     * A async function to fetch the data 
+     * @param {*} method 
+     * @returns 
+     */
+    async function fetchJSON(method = "GET") {
 
+      // check if there is an identifer
+      if (!id) {
+        setData("Please enter the identifier!");
+        return
+      }
+
+      const headers = { "Accept": "application/json" };
+      const url = _solr_base + encodeURIComponent(id) + `?format=full`;
+
+      try {
+        await fetch(url, {
+          method: method,
+          headers: headers
+        })
+          .then(res => res.json())
+          .then(data => {
+            setData("");
+            // add jsonld script into head
+            const script = document.createElement('script');
+            script.type = "application/ld+json";
+            script.text = JSON.stringify(data, null, 4);
+            document.head.appendChild(script);
+
+            // modify the current div element 
+            container.current.appendChild(window.prettyPrint(data))
+          })
+      } catch (err) {
+        console.log("Error: ", err);
+        setData("Wrong Identifier!");
+      }
+    }
+    fetchJSON();
+  }, [id]);
 
   return (
     <>
-      <JsonToTable json={jsonData} />
+      {jsonData}
+      <div ref={container} />
     </>
   )
-
 }
 
 export default App;
