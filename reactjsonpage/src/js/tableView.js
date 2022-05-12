605dc25dad9c9c22f9a133466bced3ce46789e08
qgan7125 <qgan@email.arizona.edu>
2022-05-11 18:43:03 -0700

    update string manipulation

--- reactjsonpage/src/js/tableView.js
+++ reactjsonpage/src/js/tableView.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import HtmlJsonTable from "react-json-to-html-table"
 import "../css/table.css";
 const fields = ["id", "authority_id", "primary_key", "item_type", "resolved_content"];
@@ -6,24 +5,40 @@ const fields = ["id", "authority_id", "primary_key", "item_type", "resolved_cont
 function TableView(props) {
     const { data } = props;
 
+    // filter the original json data to include only specify fields.
     const selectedFields = Object.fromEntries(Object.entries(data).filter(([key]) => fields.includes(key)));
 
+    /**
+     * A recusion method to concatenate the parent key and children key
+     *  
+     * @param {*} prevKey, the parent key
+     * @param {*} jsonData, the current json data
+     * @returns 
+     */
     function createTable(prevKey, jsonData) {
         let result = {};
         for (let key of Object.keys(jsonData)) {
+            // remove the empty fields
             if (jsonData[key].length === 0) {
                 continue;
             }
             const newKey = prevKey + " " + key;
+
+            // if the child is a hashtable or array
+            // deal with the child object
             if (typeof jsonData[key] === 'object') {
+                // if the child is an arrary
                 if (Array.isArray(jsonData[key])) {
+                    // concatenate pure string array
                     if (typeof jsonData[key][0] === 'string') {
                         result[newKey] = jsonData[key].join(', ');
                     } else {
-                        let list = jsonData[key].map(v => JSON.stringify(v)).join('\r\n')
+                        // if the value is object array, do string manipulation
+                        const list = jsonData[key].map(v => JSON.stringify(v).replaceAll(/"|{|}/g, "")).join('\r\n')
                         result[newKey] = list;
                     }
                 } else {
+                    // do recursion to children 
                     result = Object.assign({}, result, createTable(newKey, jsonData[key]));
                 }
             } else {
@@ -33,11 +48,17 @@ function TableView(props) {
         return result;
     }
 
+    /**
+     * A function to manipulate the field keys
+     * @param {*} key a string of field key
+     * @returns 
+     */
     function wellFormatKey(key) {
         const newKey = key.replace('resolved_content', "").replace('description', "").replace('supplementMetadata', "").replace(" ", "");
         return newKey.split(' ').map(word => word.slice(0, 1).toUpperCase() + word.slice(1)).join(' ');
     }
 
+    // apply functions to manipulate the json data
     const newFieldsDict = createTable("", selectedFields)
     const updateKeyDict = {}
     for (let key in newFieldsDict) {
@@ -46,7 +67,9 @@ function TableView(props) {
     }
 
     return (
-        <HtmlJsonTable data={updateKeyDict} className=" table-sm table-striped table-bordered table-responsive table-hover " />
+        <div style={{width: "800px"}}>
+            <HtmlJsonTable data={updateKeyDict} className=" table-sm table-striped table-bordered table-responsive table-hover " />
+        </div>
     )
 }
 
